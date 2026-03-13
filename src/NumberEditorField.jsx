7bcbe8a611b467216e0cede5e8ab8bb6d6b0765e
Chris3rd <Chrisquirk10@hotmail.com>
2026-03-13 11:42:01 +0000

    EDIT - Updates lots!

--- src/NumberEditorField.jsx
+++ src/NumberEditorField.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NumberEditorField = (props) => {
     return (
-        <input type="number"
+        <input type="number" className='wider'
             id="num"
             value={props.value}
             onChange={(event) => { props.valueChanged(+event.target.value); }}
